449f8b0a400abfa7627f9fc4a3c3934b8b218f6a
Luiz Fernando Duarte Junior <luizfduartejr@MacBook-Pro-de-Luiz.local>
2020-11-08 16:43:44 -0300

    blacklist e readme

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Exemplo de Web API Node.js com autenticação via JWT
 Para fazer funcionar esta aplicação, rode um "npm install" via terminal, dentro da pasta do projeto, visando instalar as dependências. Depois execute com "node index".
 
 Para o tutorial completo, acesse: https://www.luiztools.com.br/post/autenticacao-json-web-token-jwt-em-nodejs/
+
+Para um curso completo, acesse: https://www.luiztools.com.br/curso-fullstack
+
+Para um livro completo, acesse: https://www.luiztools.com.br/livro-node-ii
+
+Me siga nas redes sociais: https://about.me/luiztools
--- index.js
+++ index.js
@@ -1,58 +1,60 @@
 //index.js
-require("dotenv-safe").config(); 
-var jwt = require('jsonwebtoken');
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
 
-var http = require('http'); 
+const http = require('http');
 const express = require('express');
 const app = express();
-var cookieParser = require('cookie-parser'); 
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+const blacklist = [];
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(cookieParser()); 
 
 app.get('/', (req, res, next) => {
-    res.json({message: "Tudo ok por aqui!"});
+  res.json({ message: "Tudo ok por aqui!" });
 })
 
-app.get('/clientes', verifyJWT, (req, res, next) => { 
-    console.log("Retornou todos clientes!");
-    res.json([{id:1,nome:'luiz'}]);
-}) 
+app.get('/clientes', verifyJWT, (req, res, next) => {
+  console.log("Retornou todos clientes!");
+  res.json([{ id: 1, nome: 'luiz' }]);
+})
 
-function verifyJWT(req, res, next){
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token, process.env.SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-      
-      // se tudo estiver ok, salva no request para uso posterior
-      req.userId = decoded.id;
-      next();
-    });
+function verifyJWT(req, res, next) {
+  var token = req.headers['x-access-token'];
+  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+  const index = blacklist.findIndex(item => item === token);
+  if(index !== -1) return res.status(401).end();
+
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+    if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
+
+    // se tudo estiver ok, salva no request para uso posterior
+    req.userId = decoded.id;
+    next();
+  });
 }
 
-//authentication
 app.post('/login', (req, res, next) => {
-    //esse teste abaixo deve ser feito no seu banco de dados
-    if(req.body.user === 'luiz' && req.body.pwd === '123'){
-      //auth ok
-      const id = 1; //esse id viria do banco de dados
-      var token = jwt.sign({ id }, process.env.SECRET, {
-        expiresIn: 300 // expires in 5min
-      });
-      return res.json({ auth: true, token: token });
-    }
-    
-    res.status(500).json({message: 'Login inválido!'});
+  //esse teste abaixo deve ser feito no seu banco de dados
+  if (req.body.user === 'luiz' && req.body.pwd === '123') {
+    //auth ok
+    const userId = 1; //esse id viria do banco de dados
+    const token = jwt.sign({ userId }, process.env.SECRET, {
+      expiresIn: 300 // expires in 5min
+    });
+    return res.json({ auth: true, token: token });
+  }
+
+  res.status(401).json({ message: 'Login inválido!' });
 })
 
-app.post('/logout', function(req, res) {
-    res.json({ auth: false, token: null });
+app.post('/logout', function (req, res) {
+  blacklist.push(req.headers['x-access-token']);
+  res.json({ auth: false, token: null });
 })
 
-var server = http.createServer(app); 
+const server = http.createServer(app);
 server.listen(3000);
 console.log("Servidor escutando na porta 3000...")
